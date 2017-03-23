c43819e7895681f32898bd25a99ce23849f6394b
rickwoody86 <rickwoody86@gmail.com>
2017-03-23 17:58:17 -0400

    final

---
+++ assets/javascript/app.js
@@ -0,0 +1,156 @@
+var panel = $('#quiz-area');
+var countStartNumber = 30;
+
+///On Click Events
+
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
+
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
+
+$(document).on('click', '#start', function(e) {
+  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+  game.loadQuestion();
+});
+
+//Question Array
+
+var questions = [
+  {
+    question: "What is the real name of the Hulk?",
+    answers: ["Clark Kent", "Bruce Banner", "Carl Sagen", "Peter Parker"],
+    correctAnswer: "Bruce Banner",
+    image:"assets/images/hulk.gif"
+  },
+
+  {
+    question: "Who was not in the original X-men?",
+    answers: ["Cyclops", "Beast", "Wolverine", "Angel"],
+    correctAnswer: "Wolverine",
+    image:"assets/images/xmen.gif"
+  },
+
+  {
+    question: "Before Spider-Man got his own comic which series was he in?",
+    answers: ["Tales to Astonish", "Superhero Stories", "Marvel Heroes", "Amazing Fantasy"],
+    correctAnswer: "Amazing Fantasy",
+    image:"assets/images/spiderman.gif"
+  },
+
+  {
+    question: "What nationality is Wolverine?",
+    answers: ["Canandian", "American", "Australian", "Russian"],
+    correctAnswer: "Canandian",
+    image:"assets/images/wolverine.gif"
+  },
+
+  {
+    question: "Who was the first Ant-Man?",
+    answers: ["Wade Wilson", "Hank Pym", "Scott Lang", "Peter Parker"],
+    correctAnswer: "Hank Pym",
+    image:"assets/images/antman.gif"
+  }
+
+  
+];
+
+
+//Game Controllers 
+
+var game = {
+  questions:questions,
+  currentQuestion:0,
+  counter:countStartNumber,
+  correct:0,
+  incorrect:0,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIME UP');
+      game.timeUp();
+    }
+  },
+  loadQuestion: function(){
+    timer = setInterval(game.countdown, 1000);
+    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    game.counter = countStartNumber;
+    $('#counter-number').html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+  timeUp: function (){
+    clearInterval(timer);
+    $('#counter-number').html(game.counter);
+
+    panel.html('<h2>Took too Long</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function() {
+    clearInterval(timer);
+
+    panel.html('<h2>Results</h2>');
+    $('#counter-number').html(game.counter);
+    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+    panel.append('<h3>Wrong Answers: ' + game.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+    panel.append('<br><button id="start-over">Try Again?</button>');
+  },
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function() {
+    game.incorrect++;
+    clearInterval(timer);
+    panel.html('<h2>Sorry!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    game.correct++;
+    panel.html('<h2>Correct!</h2>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function(){
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
+  }
+};
